3636490e304dd47348c924e57e8107b111fe8e21
nestornia <nestor.nia@gmail.com>
2020-12-08 21:23:31 -0600

    Adding the Paging Source and Data pieces for the application.

--- app/src/main/java/com/example/imagesearchapplication/data/repository/UnsplashRepository.kt
+++ app/src/main/java/com/example/imagesearchapplication/data/repository/UnsplashRepository.kt
@@ -1,9 +1,23 @@
 package com.example.imagesearchapplication.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
+import com.example.imagesearchapplication.data.UnsplashPagingSource
 import com.example.imagesearchapplication.data.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = {UnsplashPagingSource(unsplashApi, query)}
+        ).liveData
 }
