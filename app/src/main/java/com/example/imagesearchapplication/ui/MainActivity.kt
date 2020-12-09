1871b0a64fdfe5fd77955ee16f70940810361836
nestornia <nestor.nia@gmail.com>
2020-12-08 20:26:36 -0600

    Adding navigation to the fragments

--- app/src/main/java/com/example/imagesearchapplication/ui/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchapplication/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.imagesearchapplication
+package com.example.imagesearchapplication.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.imagesearchapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
